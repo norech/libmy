cce13e5335865665cc746297ea868f2098fd345a
Alexis Cheron <alexisnorech@gmail.com>
2020-12-23 19:26:36 +0400

    fixed my_array_count for one byte element size

--- src/my_array/my_array_count.c
+++ src/my_array/my_array_count.c
@@ -12,7 +12,10 @@ int my_array_count(size_t element_size, void *array)
     int len = 0;
     unsigned int nullbytes_count = 0;
     unsigned char *bytes = (unsigned char *)array;
+
     for (int i = 0; true; i++) {
+        if (element_size == 1 && bytes[i] == 0)
+            break;
         len++;
         if (bytes[i] == 0)
             nullbytes_count++;
@@ -22,6 +25,5 @@ int my_array_count(size_t element_size, void *array)
         if (bytes[i] != 0 || i % element_size == 0)
             nullbytes_count = 0;
     }
-    DEBUG_PRINTF("%d", len % element_size);
     return (len / element_size);
 }
