c2c0ff2394acadec5e1a44bbd44567aadd489898
Alexis Cheron <alexisnorech@gmail.com>
2021-01-22 09:17:01 +0400

    fixed strstr and string indexof

--- src/my_str/my_strstr.c
+++ src/my_str/my_strstr.c
@@ -9,14 +9,15 @@
 
 strpos_t my_strstr(str_t str, str_t to_find)
 {
-    if (str[0] == '\0' && to_find[0] == '\0') {
-        return (&str[0]);
-    }
-
     int i = 0;
+    int j = 0;
+    bool is_end;
+
+    if (my_strncmp(str, to_find, my_strlen(to_find)) == 0)
+            return (&str[0]);
     while (str[i] != '\0') {
-        int j = 0;
-        int is_end = (to_find[j] == '\0' || str[i + j] == '\0');
+        j = 0;
+        is_end = (to_find[j] == '\0' || str[i + j] == '\0');
         while (!is_end && str[i + j] == to_find[j]) {
             j++;
         }
--- src/my_string/my_string_indexof.c
+++ src/my_string/my_string_indexof.c
@@ -8,10 +8,13 @@
 #include <my/string.h>
 #include <my/str.h>
 #include <my/types.h>
+#include <my.h>
 
 isize_t my_string_indexof(string_t const *self, string_t const *str)
 {
     strpos_t pos = my_strstr(self->as_str, str->as_str);
 
+    if (pos == NULL)
+        return (-1);
     return (pos - self->as_str);
 }
